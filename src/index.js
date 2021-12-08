0cf427283f2a2bc75c799b256f84d27204e69b7b
choewy <70950533+choewy@users.noreply.github.com>
2021-12-08 14:39:56 +0900

    store.js -> redux.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import ReduxProvider from './store';
+import ReduxProvider from './redux';
 
 ReactDOM.render(
   <ReduxProvider>
